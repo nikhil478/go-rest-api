82e0fa83f6232c53989393c6e1d2245e9ade0708
nikhil478 <nikhilmatta45@gmail.com>
2025-07-10 01:52:26 +0530

    added chi

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/nikhil478/go-rest-api
 
 go 1.24.0
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/go-chi/cors v1.2.2
+)
--- internal/http/server.go
+++ internal/http/server.go
@@ -3,18 +3,41 @@ package http
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func StartHTTPServer() {
-	handler := http.ServeMux{}
 
-	handler.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch,
+			http.MethodOptions, http.MethodDelete, http.MethodHead, http.MethodTrace},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	}))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/admin", func(r chi.Router) {
+		r.Route("/supplier", func(r chi.Router) {
+			r.Post("/", func(w http.ResponseWriter, r *http.Request) {})
+			r.Get("/{supplierID}", func(w http.ResponseWriter, r *http.Request) {})
+		})
+	})
+
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World !"))
 	})
 
 	log.Default().Printf("http server started at %s", ":8080")
 
-	if err := http.ListenAndServe(":8080", &handler); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("error while init http server on port %s", ":8080")
 	}
 }
