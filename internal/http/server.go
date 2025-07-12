0db7a3723dad45cfe34f5276a8469a254d55355e
nikhil478 <nikhilmatta45@gmail.com>
2025-07-12 21:11:13 +0530

    added endpoints

--- internal/http/server.go
+++ internal/http/server.go
@@ -13,29 +13,24 @@ func StartHTTPServer() {
 
 	r := chi.NewRouter()
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch,
-			http.MethodOptions, http.MethodDelete},
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodOptions, http.MethodDelete},
 		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
 	}))
+	
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Route("/admin", func(r chi.Router) {
-		r.Route("/supplier", func(r chi.Router) {
-			r.Post("/", func(w http.ResponseWriter, r *http.Request) {})
-			r.Get("/{supplierID}", func(w http.ResponseWriter, r *http.Request) {})
-		})
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", func(w http.ResponseWriter, r *http.Request) {})
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {})
+		r.Get("/{userID}", func(w http.ResponseWriter, r *http.Request) {})
+		r.Put("/{userID}", func(w http.ResponseWriter, r *http.Request) {})
+		r.Delete("/{userID}", func(w http.ResponseWriter, r *http.Request) {})
 	})
-
-	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World !"))
-	})
-
-	log.Default().Printf("http server started at %s", ":8080")
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatalf("error while init http server on port %s", ":8080")
